c5fe750f69b24488245ee61ef1bfdb4b1717c5b3
TylerSalvato <133399190+TylerSalvato@users.noreply.github.com>
2023-09-14 01:25:16 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+developer = {
+    "first": "Tyler",
+    "last": "Salvato",
+    "age": 28,
+    "email": "tylersalvato727@gmail.com",
+    "hobbies": ["code", "music"],
+    "address": {
+        "num": 1017,
+        "street": "athens",
+        "city": "raleigh"
+    }
+}
